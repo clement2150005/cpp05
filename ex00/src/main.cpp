195fe89345577398e2b8b6bfd5df05dae2f88254
clement2150005 <clmnt.co@gmail.com>
2025-05-15 10:09:30 +0900

    Quick commit

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,31 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ccolin <ccolin@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/12 10:10:01 by ccolin            #+#    #+#             */
-/*   Updated: 2025/04/12 15:35:15 by ccolin           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <iostream>
-#include "Bureaucrat.hpp"
-
-int main(void)
+int	main(int argc, char **argv)
 {
-    try
-    {
-        Bureaucrat markScout("Mark Scout", 1);
-        while (1)
-        {
-            std::cout << markScout;
-            markScout.decrementGrade();
-        }
-    } catch (const std::exception &e)
-    {
-        std::cout << "Caught: " << e.what() << std::endl;
-    }
-    return (0);
+	(void)argc;
+	(void)argv;
+
+	
+
+	return (0);
 }
